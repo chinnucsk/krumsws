070afc403675869048809c41a43658b534eb6954
Dan Cutting <dcutting@worldjourney.com>
2010-07-22 17:54:29 +0100

    Trying to figure out JSON, objects, etc.

--- src/krumsws_products.erl
+++ src/krumsws_products.erl
@@ -13,13 +13,13 @@ content_types_provided(ReqData, State) ->
     {[{"application/json", to_json}], ReqData, State}.
 
 to_json(ReqData, State) ->
-    { io_lib:format("[
-    {
-        \"name\": \"Krums-iPhone\",
-        \"product\": \"~s/products/krums-iphone\"
-    },
-    {
-        \"name\": \"krumsws\",
-        \"product\": \"~s/products/krumsws\"
-    }
-]", [krumsws_utility:url_prefix(), krumsws_utility:url_prefix()]), ReqData, State}.
+	{ok, Pid} = riakc_pb_socket:start_link(krumsws_utility:riak_host(), krumsws_utility:riak_port()),
+	{ok, [Products]} = riakc_pb_socket:mapred_bucket(Pid, <<"products">>, [{map, {modfun, riak_kv_mapreduce, map_object_value}, none, true}]),
+%	{ok, Products} = riakc_pb_socket:mapred(Pid, [{<<"products">>, <<"krumsws">>}], [{map, {modfun, riak_kv_mapreduce, map_object_value}, none, true}]),
+%	{iolist_to_binary(mochijson2:encode([Products])), ReqData, State}.
+%	[P1|_] = Products,
+	{0, [D|_]} = Products,
+	X = mochijson2:decode(D),
+	Y = mochijson2:encode(X),
+	io:format(standard_error, "~p", [[Y]]),
+	{"stuff", ReqData, State}.
